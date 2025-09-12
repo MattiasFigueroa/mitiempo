67ae3bdb2bf0e550324618c4bb9c9f4d0102f3f2
MattiasFigueroa <mathyaz77@gmail.com>
2025-09-12 02:38:05 -0300

    algunos retoques

--- frontend/mitiempo_react/src/App.js
+++ frontend/mitiempo_react/src/App.js
@@ -18,6 +18,17 @@ function App() {
           imagen=""
 
         />
+        <NombreComponente  /* crea el componente y lo renderiza en el navegador */
+          /* a continuacion escribimos los props que usara este componente con sus valores */
+          /* ACLARACION: estos props los tiene que traer de una CRUD/BD */
+          nombre="Emma Bostian"
+          pais="Argentina"
+          cargo="Analista de Sistemas"
+          empresa="Despegar"
+          testimonio="La función principal de un analista de sistemas es analizar, diseñar, implementar y mejorar sistemas informáticos para que las empresas sean más eficientes y cumplan con sus objetivos de negocio. Para lograrlo, investigan problemas empresariales, desarrollan soluciones tecnológicas, colaboran con equipos de desarrollo, forman a usuarios y se aseguran de que los sistemas sean seguros y estén actualizados. "
+          imagen=""
+
+        />
       </div>
     </div>
   );
